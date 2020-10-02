26ee4a145c9dc9a2b06b13a7aa9b9a524e3e0028
mantuonweb <mantuonweb@hotmail.com>
2020-10-02 14:43:58 +0530

    home changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
   // {path: '**', redirectTo: '/aboutus'}
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false, useHash: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
